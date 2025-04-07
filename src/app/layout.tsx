fc18f4bdfce914a897bb86beaf5329717783e9d9
NgnPhamGiaHuy <yuh.nguyenpham@gmail.com>
2025-04-07 21:41:31 +0700

    feat(auth): add initial UI layouts for sign-in and sign-up pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monaSans = Mona_Sans({
+    variable: "--font-mona-sans",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "MockInter",
+    description: "An AI-powered platform for preparing for mock interviews.",
+};
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en" className={"dark"}>
+            <body className={`${monaSans.variable} antialiased pattern`} suppressHydrationWarning>
+                { children }
+                <Toaster/>
+            </body>
+        </html>
+    );
+}
